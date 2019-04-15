130d69944acaf157bf84aa2bcc9f49596136979a
Stefan Muenzel <source@s.muenzel.net>
2019-04-15 19:26:37 +0800

    try to create some ocaml

--- bin/gen_vopt.ml
+++ bin/gen_vopt.ml
@@ -3,9 +3,18 @@ open! Core
 module S = Cmm_vopt.Subcmm
 
 let main () =
-  Cmm_vopt.Schemes.example
-  |> S.smt_of_t
-  |> List.iter ~f:print_s
+  let example = Cmm_vopt.Schemes.example in
+  let () =
+    example
+    |> S.smt_of_t
+    |> List.iter ~f:print_s
+  in
+  let () =
+    example.input
+    |> S.Expr.cmm_pat_of_t
+    |> print_string
+  in
+  ()
 
 let (command : Command.t) =
   let open Command.Let_syntax in
--- src/subcmm.ml
+++ src/subcmm.ml
@@ -10,6 +10,11 @@ module Const = struct
     | Zero    -> Sexp.Atom "#b00000"
     | One     -> Sexp.Atom "#b00001"
     | Neg_one -> Sexp.Atom "#b11111"
+
+  let ocaml_of_t = function
+    | Zero -> "0"
+    | One  -> "1"
+    | Neg_one -> "-1"
 end
 
 module Op = struct
@@ -39,10 +44,20 @@ module Op = struct
     | Lsr -> Sexp.Atom "ocaml-lsr"
     | Asr -> Sexp.Atom "ocaml-asr"
 
+  let cmm_of_int_op = function
+    | Lsl -> "Clsl"
+    | Lsr -> "Clsr" 
+    | Asr -> "Casr"
+
   let smt_of_int_log_op = function
     | Or -> Sexp.Atom "OR"
     | And -> Sexp.Atom "AND"
     | Xor -> Sexp.Atom "XOR"
+
+  let cmm_of_int_log_op = function
+    | Or -> "Cor"
+    | And -> "Cand"
+    | Xor -> "Cxor"
 
   let smt_of_int_cmp_op = function
     | EQ -> Sexp.Atom "EQ"
@@ -51,6 +66,14 @@ module Op = struct
     | GT -> Sexp.Atom "GT"
     | LE -> Sexp.Atom "LE"
     | GE -> Sexp.Atom "GE"
+
+  let cmm_of_int_cmp_op = function
+    | EQ -> "Ccmpi Ceq"
+    | NE -> "Ccmpi Cne"
+    | LT -> "Ccmpi Clt"
+    | GT -> "Ccmpi Cgt"
+    | LE -> "Ccmpi Cle"
+    | GE -> "Ccmpi Cge"
 
   let smt_of_t = function
     | Tagi -> [ Sexp.Atom "ocaml-tagi" ]
@@ -68,6 +91,9 @@ module Var = struct
   end
   include T
   include Comparator.Make(T)
+
+  let ocaml_of_t { name ; _ } =
+    name
 
   let smt_of_t { name ; _ } =
     Sexp.Atom name
@@ -89,6 +115,13 @@ module Var = struct
     }
 end
 
+module Var_action = struct
+  type t =
+    | Plain
+    | Tag
+    | Untag
+end
+
 module Expr = struct
   type t =
     | Const of Const.t
@@ -97,6 +130,28 @@ module Expr = struct
         { op : Op.t
         ; args : t list
         }
+
+  let rec cmm_pat_of_t = function
+    | Const c ->
+      sprintf "(Cconst_int (%s,_))" (Const.ocaml_of_t c)
+    | Var v ->
+      Var.ocaml_of_t v
+    | Op { op; args } ->
+      let args = 
+        List.map args ~f:cmm_pat_of_t
+        |> String.concat ~sep:"; "
+      in
+      match op with
+      | Tagi ->
+        sprintf "(Cop(Caddi,[Cop(Clsl, [%s; Cconst_int(1,_)],_); Cconst_int(1,_)],_))"
+          args
+      | I intop ->
+        sprintf "(Cop(%s,[%s],_))" (Op.cmm_of_int_op intop) args
+      | LI logop ->
+        sprintf "(Cop(%s,[%s],_))" (Op.cmm_of_int_log_op logop) args
+      | CI cmpop ->
+        sprintf "(Cop(%s,[%s],_))" (Op.cmm_of_int_cmp_op cmpop) args
+
 
   let rec smt_of_t = function
     | Const c ->
@@ -140,11 +195,8 @@ let assert_not a b =
         ]
     ]
 
-let push i =
-  Sexp.List [ Sexp.Atom "push"; Int.sexp_of_t i ]
-
-let pop i =
-  Sexp.List [ Sexp.Atom "pop"; Int.sexp_of_t i ]
+let push i = [%message "push" ~_:(i : int)]
+let pop i = [%message "pop" ~_:(i : int)]
 
 let push_check_pop v =
   List.concat
@@ -160,7 +212,8 @@ let smt_of_t
     { input
     ; output
     ; constraints = _
-    } =
+    }
+  =
   let constraints =
     let all_vars =
       Set.union (Expr.vars input) (Expr.vars output)
